153125fbb3a079dcd9574fb1acb10608e60d5c9f
unknown <afiqakimy123@gmail.com>
2026-01-31 17:15:02 +0800

    test commit

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -60,10 +60,8 @@ function MyForm() {
 
     return (
         <form>
-            <label>Enter your name:
-                <input type="text"  value={name} onChange={(e) => setName(e.target.value)} />
-            </label>
-            <p>Current value: {name}</p>
+            <input type="text"  value={name} onChange={(e) => setName(e.target.value)} />
+            <p>{name}</p>
         </form>
     )
 }
